8b5c9646af76a6780abcbd1ce08c21c9feb0c5e0
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-01-27 17:55:32 -0700

    Removed ERC721 and fixed DAI addresses

--- src/PhantasmManager.sol
+++ src/PhantasmManager.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8;
 
 // Some interfaces are needed here
 import "./interfaces/IERC20.sol";
-import "../lib/solmate/src/tokens/ERC721.sol";
 import './interfaces/ISwapImplementation.sol';
 
 
@@ -19,9 +18,11 @@ interface bondImplementation {
     function buyYieldTokens (address _assetPool, uint64 _depositId, uint256 _stableInAmount) external returns (uint64 _fundingID, uint256 fundingMultitokensMinted, uint256 actualFundAmount, uint256 principalFunded);
 }
 
-contract PhantasmManager is ERC721 {
+contract PhantasmManager {
 
     address private owner;
+    address private immutable DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address private immutable DInterest = 0x6D97eA6e14D35e10b50df9475e9EFaAd1982065E;
 
     struct Position {
         bool    isLong;
@@ -35,40 +36,9 @@ contract PhantasmManager is ERC721 {
     }
     // Ledger holds all token ids to tokens
     mapping(uint256 => Position) private positionLedger;
-    uint256 counter = 0;
-    constructor() ERC721("Phantasm Position", "SPCTR") {
-        owner = msg.sender;
-    }
 
-    /*
-            __      __         _ _     ______                _   _                 
-            \ \    / /        | | |   |  ____|              | | (_)                
-            \ \  / /_ _ _   _| | |_  | |__ _   _ _ __   ___| |_ _  ___  _ __  ___ 
-            \ \/ / _` | | | | | __| |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
-            \  / (_| | |_| | | |_  | |  | |_| | | | | (__| |_| | (_) | | | \__ \
-            \/ \__,_|\__,_|_|\__| |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
-                                                                                
-                                                                                
-    */
-    function addPosition(Position memory _newPosition) internal returns(uint256) {
-        counter++;
-        //Push Position to ledger
-        positionLedger[counter] = _newPosition;
 
-        _mint(msg.sender, counter);
-        
-        return counter;
-    }
     
-    function removePosition(uint256 _tokenID) internal {
-        _burn(_tokenID);
-
-        delete positionLedger[_tokenID];
-    }
-    function viewPosition(uint256 _tokenID) public view returns(Position memory) {
-        return positionLedger[_tokenID];
-    }
-
 
     /*
       _____                 _                           _        _   _                 
@@ -135,7 +105,7 @@ contract PhantasmManager is ERC721 {
         createdPosition.isInsulated = false;
         createdPosition.asset = _longToken;
         createdPosition.debtOwed = totalBorrow;
-        createdPosition.stablecoin = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI for now
+        createdPosition.stablecoin = DAI; // DAI for now
         createdPosition.totalCollateral = totalCollateral;
         createdPosition.lender = _lenderImplementation;
 
@@ -145,14 +115,14 @@ contract PhantasmManager is ERC721 {
     }
 
     function closeLongPosition(uint256 _tokenID, uint8 _swapImplementation, uint256 _interestAccured) public {
-            require(ownerOf(_tokenID) == msg.sender, "You have to own something to get it's value");
+            // Check removed for now require(ownerOf(_tokenID) == msg.sender, "You have to own something to get it's value");
             Position memory liquidateMe = viewPosition(_tokenID);
             //swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to)
             // DAI to repay
             uint256 amountToRepay = _interestAccured + liquidateMe.debtOwed;
-            IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).transferFrom(msg.sender, address(this), amountToRepay);
-            IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).approve(lenderImplementations[liquidateMe.lender], amountToRepay);
-            lenderImplementation(lenderImplementations[liquidateMe.lender]).closePosition(0x6B175474E89094C44Da98b954EedeAC495271d0F, liquidateMe.asset, swapImplementations[_swapImplementation], amountToRepay, liquidateMe.totalCollateral);
+            IERC20(DAI).transferFrom(msg.sender, address(this), amountToRepay);
+            IERC20(DAI).approve(lenderImplementations[liquidateMe.lender], amountToRepay);
+            lenderImplementation(lenderImplementations[liquidateMe.lender]).closePosition(DAI, liquidateMe.asset, swapImplementations[_swapImplementation], amountToRepay, liquidateMe.totalCollateral);
 
     }
 
@@ -164,8 +134,8 @@ contract PhantasmManager is ERC721 {
         uint256 _assetAmount,
         uint256 _initialBorrow
         ) public returns (uint256) {
-            IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).transferFrom(msg.sender, address(this), _assetAmount);
-            IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).approve(lenderImplementations[_lenderImplementation], _assetAmount);
+            IERC20(DAI).transferFrom(msg.sender, address(this), _assetAmount);
+            IERC20(DAI).approve(lenderImplementations[_lenderImplementation], _assetAmount);
             (uint256 totalBorrow, uint256 totalCollateral) = lenderImplementation(lenderImplementations[_lenderImplementation]).leverageShort(_shortToken, swapImplementations[_swapImplementation], _assetAmount, _initialBorrow, _borrowFactor);
 
             Position memory createdPosition;
@@ -174,7 +144,7 @@ contract PhantasmManager is ERC721 {
             createdPosition.isInsulated = false;
             createdPosition.asset = _shortToken;
             createdPosition.debtOwed = totalBorrow;
-            createdPosition.stablecoin = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI for now
+            createdPosition.stablecoin = DAI; // DAI for now
             createdPosition.totalCollateral = totalCollateral;
             createdPosition.lender = _lenderImplementation;
 
@@ -190,7 +160,7 @@ contract PhantasmManager is ERC721 {
             uint256 amountToRepay = _interestAccured + liquidateMe.debtOwed;
             IERC20(liquidateMe.asset).transferFrom(msg.sender, address(this), amountToRepay);
             IERC20(liquidateMe.asset).approve(lenderImplementations[liquidateMe.lender], amountToRepay);
-            lenderImplementation(lenderImplementations[liquidateMe.lender]).closePosition(liquidateMe.asset, 0x6B175474E89094C44Da98b954EedeAC495271d0F, swapImplementations[_swapImplementation], amountToRepay, liquidateMe.totalCollateral);
+            lenderImplementation(lenderImplementations[liquidateMe.lender]).closePosition(liquidateMe.asset, DAI, swapImplementations[_swapImplementation], amountToRepay, liquidateMe.totalCollateral);
 
     }
 
@@ -216,9 +186,9 @@ contract PhantasmManager is ERC721 {
         IERC20(_longToken).transferFrom(msg.sender, address(this), _assetAmount);
 
                 // Insure against DAI you will be borrowing
-        IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).transferFrom(msg.sender,bondImplementations[0], stableFundAmount);      
+        IERC20(DAI).transferFrom(msg.sender,bondImplementations[0], stableFundAmount);      
 
-        bondImplementation(bondImplementations[0]).buyYieldTokens(0x6D97eA6e14D35e10b50df9475e9EFaAd1982065E, _depositId, stableFundAmount);
+        bondImplementation(bondImplementations[0]).buyYieldTokens(DInterest, _depositId, stableFundAmount);
         
         IERC20(_longToken).approve(lenderImplementations[0], _assetAmount);
 
@@ -232,7 +202,7 @@ contract PhantasmManager is ERC721 {
         createdPosition.isInsulated = true;
         createdPosition.asset = _longToken;
         createdPosition.debtOwed = totalBorrow;
-        createdPosition.stablecoin = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI for now
+        createdPosition.stablecoin = DAI; // DAI for now
         createdPosition.totalCollateral = totalCollateral;
         createdPosition.lender = 0;
 
@@ -246,13 +216,13 @@ contract PhantasmManager is ERC721 {
             // _tipFee is just incase bond doesn't accure exactly the same and you need to give it a lil boost
             Position memory liquidateMe = viewPosition(_tokenID);
             require(liquidateMe.isInsulated);
-            IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).transferFrom(msg.sender, address(this), liquidateMe.debtOwed + _tipFee);
+            IERC20(DAI).transferFrom(msg.sender, address(this), liquidateMe.debtOwed + _tipFee);
             uint256 amountCollected = bondImplementation(bondImplementations[_bondImplementation]).collectAllInterest(liquidateMe.stablecoin);
             //swap(address _tokenIn, address _tokenOut, uint _amountIn, uint _amountOutMin, address _to)
             // DAI to repay
             uint256 amountToRepay = amountCollected + liquidateMe.debtOwed + _tipFee;
-            IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F).approve(lenderImplementations[liquidateMe.lender], amountToRepay);
-            lenderImplementation(lenderImplementations[liquidateMe.lender]).closePosition(0x6B175474E89094C44Da98b954EedeAC495271d0F, liquidateMe.asset, swapImplementations[_swapImplementation], amountToRepay, liquidateMe.totalCollateral);
+            IERC20(DAI).approve(lenderImplementations[liquidateMe.lender], amountToRepay);
+            lenderImplementation(lenderImplementations[liquidateMe.lender]).closePosition(DAI, liquidateMe.asset, swapImplementations[_swapImplementation], amountToRepay, liquidateMe.totalCollateral);
     }
 
 
